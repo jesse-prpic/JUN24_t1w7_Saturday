21fc2674a4bce057c5d44c014fcd8031c1cc77b5
jesse-prpic <13812@coderacademy.edu.au>
2024-06-15 12:58:00 +1000

    completed some questions

---
+++ calculator.py
@@ -0,0 +1,41 @@
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        return "Error! Division by Zero"
+    return a / b
+
+def get_user_input():
+    operation = input("Enter operation (+, -, *, /): ")
+    if operation not in ['+', '-', '*', '/']:
+        print("Invalid Operation")
+        return None, None, None
+
+
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    return operation, a, b
+
+def main():
+    operation, num1, num2 = get_user_input()
+    result = 0
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = subtract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    else:
+        result = divide(num1, num2)
+
+    print(f"The result is: {result}")
+
+main()
